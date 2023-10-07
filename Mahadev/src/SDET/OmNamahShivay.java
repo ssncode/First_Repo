726ac4665b3cce173788a1d366705411eef02517
sasharma <sasharma@INBMWN145863.ad.harman.com>
2023-10-07 19:59:07 +0530

    FirstCommit

---
+++ Mahadev/src/SDET/OmNamahShivay.java
@@ -0,0 +1,13 @@
+package SDET;
+
+public class OmNamahShivay {
+	public static void main(String[] args) {
+		
+	} 
+	{
+		System.out.println("sachin");
+	}
+	
+	
+
+}
---
+++ Mahadev/src/SDET/Student.java
@@ -0,0 +1,29 @@
+package SDET;
+
+public class Student {
+	int RollNumber= 10;
+	int Age= 11;
+	public void display1()
+	{
+		System.out.println("Welcome to all of you");
+	
+	}
+
+	public void display2() {
+		System.out.println("Automation is very easy");
+	}
+	
+	public static void main (String[] args) {
+		
+		Student obj=new Student();
+		obj.display1();
+		obj.display2();
+		obj.Age=23;
+		System.out.println(obj.Age);
+		obj.RollNumber= 6767;
+		System.out.println(obj.RollNumber);
+		
+		
+		
+	}
+}
