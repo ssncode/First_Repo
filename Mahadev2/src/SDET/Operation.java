0d330a71ba68761974cee9c39e79468acd1058ef
ssncode <sachinsharmadelhi@hotmail.com>
2023-10-10 12:01:33 +0530

    ABCD

---
+++ Mahadev2/src/SDET/Operation.java
@@ -0,0 +1,42 @@
+package SDET;
+
+public class Operation {
+	int a;
+	int b;
+	public int sum(int a,int b)
+	{
+		int V1 =(a+b);
+		return V1;
+	}
+	
+	public int sub(int c,int d)
+	{
+		int V2= (c-d);
+		return V2;
+	}
+	public int div(int a,int b)
+	{
+		int V3= (a/b);
+		return V3;
+	}
+	public int mul(int a,int b)
+	{
+		int V4= (a*b);
+		return V4;
+		
+	}
+
+	public static void main (String[] args)
+	{
+		Operation obj= new Operation();
+		int Sam= obj.sum(10,2);
+		int Sam2= obj.sum(Sam, 2);
+		int Sam3= obj.sub (Sam2, 2);
+		int Sam4= obj.mul(Sam3, 2);
+		int Sam5= obj.div(Sam4, 2);
+		System.out.println(Sam5);
+		
+	}
+
+	
+}
